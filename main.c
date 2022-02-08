6e377044fbaf6856df62b6bf63aaee0cacf36359
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-02-07 21:16:15 -0300

    MATRIX5x5 Finalizado

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//global
+int matriz[5][5];
+int linha, coluna;
+int soma = 0;
+int somaLinhas[5];
+void *geraMatriz(){ // Funcao Matriz 5x5
+	srand(time(NULL)); // gerador de numeros aleatorios
+	for(linha=0; linha<5; linha++){
+		for(coluna=0;coluna<5;coluna++){
+			matriz[linha][coluna]=rand()%100; //aloca os numeros aleatorios
+			soma += matriz[linha][coluna]; //soma do total
+			somaLinhas[linha] += matriz[linha][coluna]; //soma de cada linha
+			printf("%d\t",matriz[linha][coluna]);
+		}
+		printf("\n");
+	}
+}
+void *somatorio(){
+	for (int linha = 0; linha < 5; linha++){
+		printf("\nA soma da linha %d = %d", linha+1, somaLinhas[linha]);
+	}
+	printf("\nA soma total = %d", soma);
+}
+int main() {
+	printf("\n\t MATRIZ 5X5\n\n");
+	geraMatriz();
+	somatorio();
+
+}
